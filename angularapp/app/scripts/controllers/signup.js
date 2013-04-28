21d3de076b671af7d838295743a323aab3df98f4
André Eife <andre.eife@gmail.com>
2013-04-28 17:33:56 +0200

    added flash message for duplicate name error on sign up

--- angularapp/app/scripts/controllers/signup.js
+++ angularapp/app/scripts/controllers/signup.js
@@ -4,13 +4,14 @@ angular.module('angularappApp')
   .controller('SignupCtrl', function ($scope, auth, flash) {
     $scope.signup = function(){
         if ($scope.signupForm.$invalid) {
-            flash.error("wrong input")
+            flash.error("wrong input");
         } else if ($scope.password != $scope.passwordConfirm) {
-            flash.error("passwords do not match")
+            flash.error("passwords do not match");
         } else {
             auth.signup($scope.username, $scope.password, function(data){
                 console.log(data);
                 $scope.error = data.error;
+                flash.error(data.error)
             });
         }
     }
