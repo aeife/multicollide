fa5358457c4e00aa21d35a6aee175f1dd99a6c0a
André Eife <andre.eife@gmail.com>
2013-05-03 15:40:45 +0200

    added toArray filter for oderBy supporting objects

--- angularapp/app/scripts/directives/friendslist.js
+++ angularapp/app/scripts/directives/friendslist.js
@@ -10,26 +10,36 @@ angular.module('angularappApp')
         // subscribe to online status changes for all friends
         // onlinestatus:<username>
         $scope.friends = {};
-        $scope.$watch(auth.isLoggedIn, function(newValue, oldValue) {
-          if (newValue) {
-            user.getUserInfo(auth.key(), function(data){
-                console.log(data.friends);
+        $scope.friends["test"] = {online:false};
+        $scope.friends["tester3"] = {online: true};
+        $scope.friends["tester9"] = {online: false};
+        // $scope.$watch(auth.isLoggedIn, function(newValue, oldValue) {
+        //   if (newValue) {
+        //     user.getUserInfo(auth.key(), function(data){
+        //         console.log(data.friends);
 
-                for (var i = 0; i < data.friends.length; i++) {
-                    $scope.friends[data.friends[i]] = {online: false};
-                    console.log($scope.friends);
-                    console.log(i);
-                    socket.on("onlinestatus:"+data.friends[i], function(sdata){
-                        console.log(i);
-                        console.log(sdata);
-                        console.log(data.friends);
-                        console.log(sdata.user + " has changed online status to: " + sdata.online);
-                        $scope.friends[sdata.user].online = sdata.online;
-                    });
-                }
-            });
-          }
-        });
+        //         for (var i = 0; i < data.friends.length; i++) {
+        //             $scope.friends[data.friends[i]] = {online: false};
+        //             console.log($scope.friends);
+        //             console.log(i);
+        //             socket.on("onlinestatus:"+data.friends[i], function(sdata){
+        //                 console.log(i);
+        //                 console.log(sdata);
+        //                 console.log(data.friends);
+        //                 console.log(sdata.user + " has changed online status to: " + sdata.online);
+        //                 $scope.friends[sdata.user].online = sdata.online;
+        //             });
+        //         }
+        //     });
+        //   }
+        // });
+
+
+        $scope.sortMe = function() {
+            return function(object) {
+                return object.value.online;
+            }
+        }
       },
     };
   });
---
+++ angularapp/app/scripts/filters/toArray.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularappApp')
+  .filter('toArray', function() { return function(obj) {
+    if (!(obj instanceof Object)) return obj;
+    return _.map(obj, function(val, key) {
+        // return Object.defineProperty(val, '$key', {__proto__: null, value: key});
+        val.$key = key; return val;
+    });
+}});
