8e9e25fa7ba81c363b2ce6a408f0976508414048
aeife <andre.eife@gmail.com>
2013-05-25 17:27:04 +0200

    watch all files for karma rerun

--- angularapp/Gruntfile.js
+++ angularapp/Gruntfile.js
@@ -45,6 +45,10 @@ module.exports = function (grunt) {
       },
       watchTests: {
         files: [
+          '<%= yeoman.app %>/{,*/}*.html',
+          '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
+          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           'test/spec/{,*/}*.js'
         ],
         tasks: ['karma']
