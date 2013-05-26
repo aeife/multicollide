f97849a9c3a66fa1928a3d84c1843940a2b03328
aeife <andre.eife@gmail.com>
2013-05-26 14:52:22 +0200

    cleanup and added refresh

--- angularapp/app/scripts/controllers/games.js
+++ angularapp/app/scripts/controllers/games.js
@@ -2,23 +2,29 @@
 
 angular.module('angularappApp')
   .controller('GamesCtrl', function ($scope, lobby) {
-  	$scope.games = null;
-  	$scope.order = "name";
-  	$scope.reverse = false;
+    $scope.games = null;
+    $scope.order = "name";
+    $scope.reverse = false;
 
-  	console.log("requesting games");
-  	lobby.getAvailableGames(function(data){
-  		console.log("controller got data");
-  		console.log(data);
-  		$scope.games = data;
-  	});
+    $scope.reorder = function(attr){
+      if (attr === $scope.order){
+        $scope.reverse = !$scope.reverse;
+      } else {
+        $scope.order = attr;
+        $scope.reverse = false;
+      }
+    }
 
-  	$scope.reorder = function(attr){
-  		if (attr === $scope.order){
-  			$scope.reverse = !$scope.reverse;
-  		} else {
-  			$scope.order = attr;
-  			$scope.reverse = false;
-  		}
-  	}
+    $scope.joinGame = function(id){
+      console.log("trying to join game with id " + id);
+    }
+
+    $scope.refresh = function(){
+      lobby.getAvailableGames(function(data){
+        console.log(data);
+        $scope.games = data;
+      });
+    }
+
+    $scope.refresh();
   });
