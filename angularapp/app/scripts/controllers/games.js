61ff4a34306b5fd258a50482194fe111270f9c2e
aeife <andre.eife@gmail.com>
2013-05-26 14:31:21 +0200

    added sorting for server browser

--- angularapp/app/scripts/controllers/games.js
+++ angularapp/app/scripts/controllers/games.js
@@ -3,6 +3,8 @@
 angular.module('angularappApp')
   .controller('GamesCtrl', function ($scope, lobby) {
   	$scope.games = null;
+  	$scope.order = "name";
+  	$scope.reverse = false;
 
   	console.log("requesting games");
   	lobby.getAvailableGames(function(data){
@@ -10,4 +12,13 @@ angular.module('angularappApp')
   		console.log(data);
   		$scope.games = data;
   	});
+
+  	$scope.reorder = function(attr){
+  		if (attr === $scope.order){
+  			$scope.reverse = !$scope.reverse;
+  		} else {
+  			$scope.order = attr;
+  			$scope.reverse = false;
+  		}
+  	}
   });
