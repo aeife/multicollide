25c03f3494a0323a43160a67bcec7051ae06168e
aeife <andre.eife@gmail.com>
2013-06-02 01:08:03 +0200

    cleanup

--- angularapp/app/scripts/filters/i18n.js
+++ angularapp/app/scripts/filters/i18n.js
@@ -3,8 +3,6 @@
 angular.module('angularappApp')
   .filter('i18n', function (localization) {
     return function (input, args) {
-      console.log(args);
-      console.log("LOCALIZING " + input);
       return localization.getLocalizedValue(input, args);
     };
   });
--- angularapp/app/scripts/services/localization.js
+++ angularapp/app/scripts/services/localization.js
@@ -20,36 +20,6 @@ angular.module('angularappApp')
         return this.language;
       },
       keys: {},
-      // generateKeys: function(callback){
-      //   for (var k in this.localization) {
-      //     this.keys[k] = k;
-      //   }
-      //   callback(this.keys);
-      // },
-      // getLocalizationKeys: function(callback){
-      //   var self = this;
-      //   if (!this.loaded)
-      //     this.init(function(){self.generateKeys(callback)});
-      //   else
-      //     this.generateKeys(callback);
-      // },
-      // changeLanguage: function(newLanguage) {
-      //   this.language = newLanguage;
-
-      //   // load new localization
-      //   this.loadLocalization();
-      // },
-      // init: function(callback){
-      //   console.log('LOADING!!');
-      //   var self = this;
-      //   $http.get('../../locale_' + this.language + '.json').success(function(data) {
-      //     console.log('loaded localization');
-      //     console.log(data);
-      //     self.localization = data;
-      //     self.loaded = true;
-      //     callback();
-      //   });
-      // },
       getLocalizationKeys: function(){
         for (var k in this.localization) {
           this.keys[k] = k;
@@ -63,7 +33,6 @@ angular.module('angularappApp')
         this.loadLocalization();
       },
       loadLocalization: function() {
-        console.log('LOADING!!');
         var self = this;
         $http.get('../../locale_' + this.language + '.json').success(function(data) {
           console.log('loaded localization');
