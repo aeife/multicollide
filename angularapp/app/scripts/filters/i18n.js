9a7c646018e66341ae2a4ca1ee2506adeb4fb406
aeife <andre.eife@gmail.com>
2013-06-01 17:10:36 +0200

    cleanup

--- angularapp/app/scripts/filters/i18n.js
+++ angularapp/app/scripts/filters/i18n.js
@@ -2,27 +2,7 @@
 
 angular.module('angularappApp')
   .filter('i18n', function (localization) {
-
-    var locale_EN = {
-      _TESTTEXT_: {
-        value: "This is a test."
-      }
-    };
-
-    var locale_DE = {
-      _TESTTEXT_: {
-        value: "Das ist ein Test."
-      }
-    };
-
-    var localize = locale_EN;
     return function (input) {
-      console.log("INPUT:");
-      console.log(input);
-      // if (!localization.loaded)
-        // localization.loadLocalization();
-      // console.log(localization.getLocalizedValue(input));
-      // return localize[input].value;
       return localization.getLocalizedValue(input);
     };
   });
