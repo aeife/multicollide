a987cfc6a969f3063c34f6bb8dcf7b74923c3715
aeife <andre.eife@gmail.com>
2013-06-05 16:56:47 +0200

    added dialog service

---
+++ angularapp/test/spec/directives/confirmDialoge.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Directive: confirmDialoge', function () {
+  beforeEach(module('angularappApp'));
+
+  var element;
+
+  // it('should make hidden element visible', inject(function ($rootScope, $compile) {
+  //   element = angular.element('<confirm-dialoge></confirm-dialoge>');
+  //   element = $compile(element)($rootScope);
+  //   expect(element.text()).toBe('this is the confirmDialoge directive');
+  // }));
+});
