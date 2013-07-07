c61d4bd1070627e3ee16abe423fcb338be52018f
aeife <andre.eife@gmail.com>
2013-07-07 10:45:10 +0200

    fixed tests

--- angularapp/test/mock/socket.js
+++ angularapp/test/mock/socket.js
@@ -13,13 +13,21 @@ angular.module('angularappAppMock.socket', [])
     this.events[eventName].push(callback);
   };
 
+  // Receive Events
+  this.onn = function(eventName, callback){
+    if(!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(callback);
+  };
+
   // Send Events
   this.emit = function(eventName, data, emitCallback){
     if(this.events[eventName]){
       angular.forEach(this.events[eventName], function(callback){
-        $rootScope.$apply(function() {
+        // $rootScope.$apply(function() {
           callback(data);
-        });
+        // });
       });
     }
     if(emitCallback) {
