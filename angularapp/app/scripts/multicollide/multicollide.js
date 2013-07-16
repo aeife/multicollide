8dbf9d968117cb4b519d8907bd739525c87e00ab
aeife <andre.eife@gmail.com>
2013-07-16 19:16:35 +0200

    added move and direction keys

--- angularapp/app/scripts/multicollide/multicollide.js
+++ angularapp/app/scripts/multicollide/multicollide.js
@@ -8,6 +8,31 @@ angular.module('multicollide', ['multicollide.level', 'multicollide.player'])
 
     level.init({canvas: canvas, ctx: ctx, wrapper: wrapper});
 
-    var p1 = new Player('red');
+    var p1 = new Player('red', "east");
     p1.spawn(1,1);
+    // window.setInterval(p1.move(), 1000);
+    setInterval(function(){
+      p1.move();
+    }, 50);
+
+    document.onkeydown = function(e) {
+      switch(e.keyCode) {
+        case 87:
+          // entspricht Taste W
+          p1.changeDirection("north");
+          break;
+        case 65:
+          // entspricht Taste A
+          p1.changeDirection("west");
+          break;
+        case 83:
+          // entspricht Taste S
+          p1.changeDirection("south");
+          break;
+        case 68:
+          // entspricht Taste D
+          p1.changeDirection("east");
+          break;
+      };
+    };
   });
--- angularapp/app/scripts/multicollide/player.js
+++ angularapp/app/scripts/multicollide/player.js
@@ -27,6 +27,73 @@ angular.module('multicollide.player', [])
         for (var i = 0; i < this.fields.length; i++){
           level.drawTile(this.fields[i].x, this.fields[i].y, this.color);
         }
+      },
+      move: function(){
+        console.log("move");
+
+        // delete tail
+        level.grid[this.fields[0].x][this.fields[0].y].player = false;
+        level.drawTile(this.fields[0].x, this.fields[0].y, 'white');
+        this.fields.shift();
+
+        // add head
+        switch (this.direction){
+          case "north":
+            var newX = this.fields[this.fields.length-1].x;
+            var newY = this.fields[this.fields.length-1].y-1;
+            if (level.isInGrid(newX, newY)){
+              this.fields.push({x: newX, y: newY});
+            }
+            else
+              this.fields.push({x: newX, y: level.gridSize-1});
+            break;
+          case "east":
+            var newX = this.fields[this.fields.length-1].x+1;
+            var newY = this.fields[this.fields.length-1].y;
+            if (level.isInGrid(newX, newY))
+              this.fields.push({x: newX, y: newY});
+            else
+              this.fields.push({x: 0, y: newY});
+            break;
+          case "south":
+            var newX = this.fields[this.fields.length-1].x;
+            var newY = this.fields[this.fields.length-1].y+1;
+            if (level.isInGrid(newX, newY))
+              this.fields.push({x: newX, y: newY});
+            else
+              this.fields.push({x: newX, y: 0});
+            break;
+          case "west":
+            var newX = this.fields[this.fields.length-1].x-1;
+            var newY = this.fields[this.fields.length-1].y;
+            if (level.isInGrid(newX, newY))
+              this.fields.push({x: newX, y: newY});
+            else
+              this.fields.push({x: level.gridSize-1, y: newY});
+            break;
+        }
+
+        level.drawTile(this.fields[this.fields.length-1].x, this.fields[this.fields.length-1].y, this.color);
+      },
+      changeDirection: function(dir){
+        switch (dir){
+          case "north":
+            if (this.direction != "south")
+              this.direction = dir;
+            break;
+          case "east":
+            if (this.direction != "west")
+              this.direction = dir;
+            break;
+          case "south":
+            if (this.direction != "north")
+              this.direction = dir;
+            break;
+          case "west":
+            if (this.direction != "east")
+              this.direction = dir;
+            break;
+        }
       }
     };
 
