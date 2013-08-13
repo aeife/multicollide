059ef92b04c9ccac69a74ec3415aeb6a63137b0e
aeife <andre.eife@gmail.com>
2013-08-13 10:08:04 +0200

    adjusted elobox for ie

--- angularapp/app/scripts/directives/eloBox.js
+++ angularapp/app/scripts/directives/eloBox.js
@@ -5,8 +5,8 @@ angular.module('eloBox', [])
     return {
       restrict: 'E',
       scope: {
-        number: "=",
-        size: "="
+        number: '=',
+        size: '='
       },
       templateUrl: 'scripts/directives/eloBox.html',
       controller: function($scope, $rootScope, $attrs){
@@ -17,10 +17,26 @@ angular.module('eloBox', [])
         }
 
         if (!$scope.number) {
-          $scope.guest = "G";
+          $scope.guest = 'G';
         } else {
           $scope.guest = undefined;
         }
+
+        $scope.getStyle = function(){
+          return {
+            'display': 'inline-block',
+            'border': $scope.borderSize + 'px solid #666',
+            'color': '#666',
+            'text-align': 'center',
+            'font-size': $attrs.size + 'px',
+            'line-height':($attrs.size * 0.8) + 'px',
+            'padding': $attrs.size * 0.2 + 'px',
+            'height': ($attrs.size * 1.2) + 'px',
+            'width': ($attrs.size * 1.6) + 'px',
+            'font-weight': 'bold',
+            'background-color': '#dddddd'
+          };
+        };
       }
     };
   });
