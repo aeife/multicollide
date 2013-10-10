5ddea915ca061b5bac78c45c21e6e813a5bc7e4c
aeife <andre.eife@gmail.com>
2013-10-10 12:59:50 +0200

    updated karma.conf

--- angularapp/karma.conf.js
+++ angularapp/karma.conf.js
@@ -1,65 +1,73 @@
 // Karma configuration
+module.exports = function(config) {
+  config.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/components/angular-resource/angular-resource.js',
-  'app/components/angular-cookies/angular-cookies.js',
-  'app/components/angular-bootstrap/ui-bootstrap.min.js',
-  'app/components/angular-placeholders/src/img/img.js',
-  'app/components/angular-placeholders/src/txt/txt.js',
-  'app/websocketApi.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  // 'test/spec/**/*.js',
-  'app/**/*.spec.js',
+    frameworks: ["jasmine"],
 
-  'app/components/underscore/underscore.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      // JASMINE,
+      // JASMINE_ADAPTER,
+      'app/components/angular/angular.js',
+      'app/components/angular-mocks/angular-mocks.js',
+      'app/components/angular-resource/angular-resource.js',
+      'app/components/angular-cookies/angular-cookies.js',
+      'app/components/angular-bootstrap/ui-bootstrap.min.js',
+      'app/components/angular-placeholders/src/img/img.js',
+      'app/components/angular-placeholders/src/txt/txt.js',
+      'app/websocketApi.js',
+      'app/scripts/*.js',
+      'app/scripts/**/*.js',
+      'test/mock/**/*.js',
+      // 'test/spec/**/*.js',
+      'app/**/*.spec.js',
 
-// list of files to exclude
-exclude = [];
+      'app/components/underscore/underscore.js'
+    ],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
 
-// web server port
-port = 8080;
+    // list of files to exclude
+    exclude: [],
 
-// cli runner port
-runnerPort = 9100;
+    // test results reporter to use
+    // possible values: dots || progress || growl
+    reporters: ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 8080,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // cli runner port
+    runnerPort: 9100,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: 'LOG_WARN',
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['ChromeCanary'],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+
+  });
+};
